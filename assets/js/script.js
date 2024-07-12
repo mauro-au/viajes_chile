c44017e002f410119aa4edb5dbc046c4a99258e4
mauro-au <mauricio.a.developer@gmail.com>
2024-07-11 21:40:10 -0400

    Se agrega una trancición de transparente a negro en el navbar al hacer scroll en la landing con js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// transición de color transparente a negro en el navbar al hacer scroll
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        if($(window).scrollTop() > 0) {
+            $('#menu').addClass('nav-color-scroll');
+        } else {
+            $('#menu').removeClass('nav-color-scroll');
+        }
+    });
+});
+
+// transición de color transparente a negro en menu mobile al hacer click en el botón de hamburguesa 
+$(".navbar-toggler").on("click", function () {
+    $(".navbar").toggleClass( "nav-color-toggler" );
+} );
