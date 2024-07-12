fb2d9e8f07671e7b81cd7689ee6085589eec141f
mauro-au <mauricio.a.developer@gmail.com>
2024-07-12 01:29:31 -0400

    Se agrega popover a las imágenes del card y se agrega js con un alert cuando se presione el botón enviar del formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// transición de color transparente a negro en el navbar al hacer scroll
 $(document).ready(function() {
+    // transición de color transparente a negro en el navbar al hacer scroll
     $(window).on('scroll', function() {
         if($(window).scrollTop() > 0) {
             $('#menu').addClass('nav-color-scroll');
@@ -10,9 +13,14 @@ $(document).ready(function() {
             $('#menu').removeClass('nav-color-scroll');
         }
     });
+    
+    // transición de color transparente a negro en menu mobile al hacer click en el botón de hamburguesa 
+    $(".navbar-toggler").on("click", function () {
+        $(".navbar").toggleClass( "nav-color-toggler" );
+    });
+    
+    // Alert al apretar botón para enviar correo 
+    $('#enviarCorreo').click(function () {
+        alert("El correo fue enviado correctamente...");       
+    })
 });
-
-// transición de color transparente a negro en menu mobile al hacer click en el botón de hamburguesa 
-$(".navbar-toggler").on("click", function () {
-    $(".navbar").toggleClass( "nav-color-toggler" );
-} );
